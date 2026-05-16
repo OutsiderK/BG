8d1eb9e0d17446ed5dc34203bd5c9beb66a45375
Kafdino <161127070+OutsiderK@users.noreply.github.com>
2026-05-16 11:01:01 +0000

    feat: 添加相机跟随与震动接口

--- game/scripts/main/main.gd
+++ game/scripts/main/main.gd
@@ -1,12 +1,19 @@
 extends Node2D
 
+const DEMO_ROOM_BOUNDS := Rect2(Vector2.ZERO, Vector2(1280, 720))
+
 @onready var player: Node = %Player
+@onready var follow_camera: Node = %FollowCamera
 
 func _ready() -> void:
+	follow_camera.call("set_follow_target", player as Node2D)
+	follow_camera.call("set_room_bounds", DEMO_ROOM_BOUNDS)
 	if player.has_signal("died"):
 		player.died.connect(_on_player_died)
+
+func shake_camera(strength := -1.0, duration := -1.0) -> void:
+	follow_camera.call("shake", strength, duration)
 
 func _on_player_died() -> void:
 	var permadeath := RunState.roll_permadeath()
 	print("Player died. Permadeath: %s" % permadeath)
-
